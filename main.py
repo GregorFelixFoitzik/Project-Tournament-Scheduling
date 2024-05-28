dbd927953b5ec69f2c41a5f6bc548703d1f7a467
jginster <jnsgnstr@jginster.de>
2024-05-28 19:28:10 +0200

    Fixed dummy merge conflict

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ parser.add_argument("path_to_instance")
 
 def test2() -> None:
     pass 
+def test1():
+    print('Helloooooo, maybe this will result in a conflict')
 
 if __name__ == '__main__':
     # Extract command line arguments
