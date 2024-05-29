3830e27762b4695525e83926b4c3dafabb2eaf3d
jginster <jnsgnstr@jginster.de>
2024-05-29 22:05:14 +0200

    Validation via tuples

--- src/validation.py
+++ src/validation.py
@@ -18,55 +18,67 @@ def validate(
     weeks = 2 * (num_teams - 1)
 
     feasible_uniqueness = uniqueness(sol=sol, num_teams=num_teams, weeks=weeks)
-    feasible_check_matches = check_matches(sol=sol, num_teams=num_teams)
+    feasible_check_games = check_games(sol=sol, num_teams=num_teams)
     feasible_each_team_monday = each_team_on_monday(sol=sol, num_teams=num_teams)
 
-    return feasible_uniqueness and feasible_check_matches and feasible_each_team_monday
+    return feasible_uniqueness and feasible_check_games and feasible_each_team_monday
 
 
 def uniqueness(sol: list[list[list[str]]], num_teams: int, weeks: int) -> bool:
     # Source: https://www.pythonpool.com/flatten-list-python/, accessed 29.05.2023
-    matches = np.unique(ar=list(itertools.chain(*list(itertools.chain(*sol)))))
+    games = np.array(list(itertools.chain(*list(itertools.chain(*sol)))))
+    games_unique = np.unique(games, axis=0)
+    games_unique = games_unique[np.sum(np.isnan(games_unique), axis=1) == 0]
+    games = games[np.sum(np.isnan(games), axis=1) == 0]
     print(
-        f"Uniqueness ({np.shape(a=matches)[0] - 1} and {int((num_teams/2)*weeks)}):"
-        f"{np.shape(a=matches)[0] - 1 == ((num_teams/2)*weeks)}"
+        f"No duplicates ({games.shape[0]} and {games_unique.shape[0]}): "
+        f"{games.shape[0] == games_unique.shape[0]}"
     )
 
-    return np.shape(a=matches)[0] == num_teams
+    # Source: https://stackoverflow.com/a/22225030, accessed 29.05.2023
+    games = games[np.invert(np.unique(np.isnan(games), axis=1).reshape(1, -1)[0])]
+    print(
+        f"Number of games ({games.shape[0]} and {int((num_teams/2)*weeks)}):"
+        f"{games.shape[0] == ((num_teams/2)*weeks)}"
+    )
+
+    return games.shape[0] == games_unique.shape[0] and (
+        games.shape[0] == num_teams
+    )
 
 
-def check_matches(sol: list[list[list[str]]], num_teams: int) -> bool:
-    matches_required = np.array(
+def check_games(sol: list[list[list[str]]], num_teams: int) -> bool:
+    games_required = np.array(
         object=list(
-            set(
                 [
-                    f"{i}vs{j}" if i != j else "-"
-                    for j in range(num_teams)
-                    for i in range(num_teams)
+                    {i, j} if i != j else [np.nan, np.nan]
+                    for j in range(1,num_teams+1)
+                    for i in range(1, num_teams + 1)
                 ]
-            )
         )
     )
-    matches_required = matches_required[matches_required != "-"]
-    matches_required = np.sort(a=matches_required)
-
+    games_required = games_required[np.sum(np.isnan(games_required), axis=1) == 0]
+    
     # Source: https://www.pythonpool.com/flatten-list-python/, accessed 29.05.2023
-    sol_flattend = np.array(object=list(itertools.chain(*list(itertools.chain(*sol)))))
-    sol_flattend = sol[sol != "-"]
-    sol_flattend = np.sort(a=matches_required)
+    sol_flatten = np.array(object=list(itertools.chain(*list(itertools.chain(*sol)))))
 
-    print(f"XvsY and YvsX: {np.unique(matches_required == sol_flattend)[0]}")
+    games_in_sol = 0
+    for game in games_required:
+        if game in sol_flatten:
+            games_in_sol += 1
 
-    return np.unique(matches_required == sol_flattend)[0]
+    print(f"XvsY and YvsX: {games_required.shape[0] == games_in_sol}")
+
+    return games_required.shape[0] == games_in_sol
 
 
 def each_team_on_monday(sol: list[list[list[str]]], num_teams: int) -> bool:
-    teams = [str(team) for team in range(1, num_teams + 1)]
+    teams = [team for team in range(1, num_teams + 1)]
     teams_on_monday = []
-    for matches_monday in sol:
-        for match in matches_monday[0]:
-            if match != "-":
-                teams_on_monday += match.split("vs")
+    for games_monday in sol:
+        for game in games_monday[0]:
+            if game != [(np.nan, np.nan)]:
+                teams_on_monday += game
 
     print(
         "Each team plays at least once "
@@ -74,6 +86,7 @@ def each_team_on_monday(sol: list[list[list[str]]], num_teams: int) -> bool:
     )
 
     return teams == np.sort(np.unique(teams_on_monday)).tolist()
+
 
 if __name__ == "__main__":
     num_teams = 6
@@ -193,16 +206,16 @@ if __name__ == "__main__":
     s = 2
 
     sol = [
-        [["5vs1"], ["3vs6"], ["2vs4"], ["-"], ["-"]],
-        [["6vs2"], ["1vs4"], ["3vs5"], ["-"], ["-"]],
-        [["5vs4"], ["1vs6"], ["-"], ["3vs2"], ["-"]],
-        [["6vs4"], ["1vs3"], ["-"], ["5vs2"], ["-"]],
-        [["1vs2"], ["3vs4"], ["5vs6"], ["-"], ["-"]],
-        [["6vs3"], ["4vs1"], ["-"], ["2vs5"], ["-"]],
-        [["3vs1"], ["4vs2"], ["-"], ["6vs5"], ["-"]],
-        [["1vs5"], ["-"], ["-"], ["4vs3"], ["2vs6"]],
-        [["4vs5"], ["2vs3"], ["-"], ["6vs1"], ["-"]],
-        [["4vs6"], ["5vs3"], ["2vs1"], ["-"], ["-"]],
+        [[{5, 1}], [{3, 6}], [{2, 4}], [{np.nan, np.nan}], [{np.nan, np.nan}]],
+        [[{6, 2}], [{1, 4}], [{3, 5}], [{np.nan, np.nan}], [{np.nan, np.nan}]],
+        [[{5, 4}], [{1, 6}], [{np.nan, np.nan}], [{5, 2}], [{np.nan, np.nan}]],
+        [[{6, 4}], [{1, 3}], [{np.nan, np.nan}], [{5, 2}], [{np.nan, np.nan}]],
+        [[{1, 2}], [{3, 4}], [{5, 6}], [{np.nan, np.nan}], [{np.nan, np.nan}]],
+        [[{6, 3}], [{4, 1}], [{np.nan, np.nan}], [{2, 5}], [{np.nan, np.nan}]],
+        [[{3, 1}], [{4, 2}], [{np.nan, np.nan}], [{6, 5}], [{np.nan, np.nan}]],
+        [[{1, 5}], [{np.nan, np.nan}], [{np.nan, np.nan}], [{4, 3}], [{2, 6}]],
+        [[{4, 5}], [{2, 3}], [{np.nan, np.nan}], [{6, 1}], [{np.nan, np.nan}]],
+        [[{4, 6}], [{5, 3}], [{2, 1}], [{np.nan, np.nan}], [{np.nan, np.nan}]],
     ]
 
     validate(
