e9b1f2e3dcfad117636dfce3e8607b756c28f017
jginster <jnsgnstr@jginster.de>
2024-06-28 21:05:00 +0200

    Added different neighborhoods for TabuSearch

--- src/metaheuristics/tabu_search.py
+++ src/metaheuristics/tabu_search.py
@@ -1,5 +1,6 @@
 # Standard library
 import os
+
 # os.environ["MKL_NUM_THREADS"] = "1"
 # os.environ['OPENBLAS_NUM_THREADS'] = '1'
 import time
@@ -11,10 +12,13 @@ from datetime import timedelta
 import numpy as np
 
 # Project specific library
+from src.validation import validate
 from src.neighborhoods import (
     insert_games_random_week,
+    reorder_week_max_profit,
+    select_n_worst_weeks,
+    select_random_weeks,
 )
-from src.validation import validate
 from src.helper import compute_profit, print_solution
 
 
@@ -35,8 +39,10 @@ class TabuSearch:
         algo_config: dict[str, Union[int, float, np.ndarray]],
         timeout: float,
         start_solution: np.ndarray,
-        rc: float, 
+        rc: float,
         max_size_tabu_list: int,
+        neighborhood: str,
+        number_of_weeks_random: int,
     ) -> None:
         self.n = int(algo_config["n"])
         self.t = float(algo_config["t"])
@@ -48,10 +54,16 @@ class TabuSearch:
         self.timeout = timeout
         self.sol = start_solution
         self.best_solution = start_solution
-        self.rc=rc 
+        self.rc = rc
 
         self.all_teams = range(1, self.n + 1)
         self.max_size_tabu_list = max_size_tabu_list
+        self.neighborhood = neighborhood
+        self.neighborhoods = {
+            "random_swap_within_week": self.random_swap_within_week,
+            "select_worst_n_weeks": self.select_worst_n_weeks,
+        }
+        self.number_of_weeks_random = number_of_weeks_random
 
     def run(self) -> np.ndarray:
         """Execute the metaheuristic.
@@ -78,29 +90,9 @@ class TabuSearch:
             and sum(os.times()[:2]) + avg_runtime + self.rc < self.timeout
         ):
             t0_iteration = time.time()
-            # Randomly select n weeks
-            possible_weeks = np.setdiff1d(
-                ar1=list(range(best_solution.shape[0])), ar2=tabu_list
+            new_sol, weeks_changed = self.neighborhoods[self.neighborhood](
+                best_solution
             )
-            number_of_weeks = min(4, possible_weeks.shape[0])
-            weeks_changed = np.random.choice(
-                a=possible_weeks,
-                size=number_of_weeks,
-                replace=False,
-            )
-            games = best_solution[weeks_changed].copy()
-
-            new_sol = best_solution.copy()
-
-            for i, week_changed in enumerate(iterable=weeks_changed):
-                week_new = insert_games_random_week(
-                    sol=new_sol,
-                    games_week=games[i],
-                    week_changed=week_changed,
-                    number_of_teams=self.n,
-                    t=self.t,
-                )
-                new_sol[week_changed] = week_new
 
             profit_new_sol = compute_profit(
                 sol=new_sol, profit=np.array(object=self.p), weeks_between=self.r
@@ -128,6 +120,73 @@ class TabuSearch:
         self.best_solution = best_solution
 
         return best_solution
+
+    def random_swap_within_week(self, sol: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """Swap the games within a week randomly.
+
+        Args:
+            sol (np.ndarray): Solution that should be changed.
+
+        Returns:
+            np.ndarray: Updated solution.
+        """
+        # Extract one random week
+        random_weeks, games_week = select_random_weeks(
+            sol=sol, number_of_weeks=self.number_of_weeks_random
+        )
+
+        new_sol = sol.copy()
+        for week in random_weeks:
+            week_new = insert_games_random_week(
+                sol=sol,
+                games_week=games_week,
+                week_changed=week,
+                number_of_teams=self.n,
+                t=self.t,
+            )
+            new_sol[week] = week_new
+
+        return new_sol, random_weeks
+
+    def select_worst_n_weeks(self, sol: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """Select the $n$-worst weeks and reorder the weeks, so profit is maximized.
+
+        Args:
+            sol (np.ndarray): Solution that should be changed.
+
+        Returns:
+            np.ndarray: Updated solution.
+        """
+        # Extract the two worst weeks
+        worst_weeks, games = select_n_worst_weeks(
+            sol=sol,
+            n=np.random.randint(low=2, high=10, size=1)[0],
+            profits=self.p,
+            weeks_between=self.r,
+        )
+
+        sol[worst_weeks] = np.full(shape=games.shape, fill_value=np.nan)
+        weeks_changed = worst_weeks
+
+        for i, week_changed in enumerate(iterable=worst_weeks):
+            week_updated = reorder_week_max_profit(
+                sol=sol,
+                profits=self.p,
+                games=games[i],
+                num_teams=self.n,
+                t=self.t,
+                current_week=week_changed,
+                weeks_between=self.r,
+            )
+
+            sol[week_changed] = week_updated
+
+        try:
+            validate(sol, self.n)
+        except Exception:
+            print("sd")
+
+        return sol, weeks_changed
 
     def check_solution(self) -> bool:
         """Check if the solutionis valued
